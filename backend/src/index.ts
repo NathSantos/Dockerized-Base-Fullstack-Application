24a6e608a20ef18706d652d47243d9e1e028aa00
Nathália Santos <nathsts@motorola.com>
2023-12-01 17:06:19 -0400

    COMMIT-04: adicionando cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import sequelize from './database/database';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import animalRoutes from './routes/AnimalRoutes';
 import { Animal } from './models/Animal';
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/animals', animalRoutes);
 
